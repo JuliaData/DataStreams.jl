8f3c5a45431af3f5db8782ea7e13f8a54195ddfc
Jacob Quinn <quinn.jacobd@gmail.com>
2015-10-23 22:56:18 -0600

    Various update

--- src/DataStreams.jl
+++ src/DataStreams.jl
@@ -11,16 +11,17 @@ The workflow enabled by the `DataStreams` framework involves:
  * continue to `Data.stream!` from `Source`s to `Sink`s
 
 The typical approach for a new package to "satisfy" the DataStreams interface is to:
- * Define a `Source` type that wraps an "ultimate data source" (i.e. a file, database table/query, etc.) and fulfills the `Source` interface (see `?Data.Source`)
- * Define a `Sink` type that can create or write data to an "ultimate data source" and fulfills the `Sink` interface (see `?Data.Sink`)
+ * Define a `Source` type that wraps an "true data source" (i.e. a file, database table/query, etc.) and fulfills the `Source` interface (see `?Data.Source`)
+ * Define a `Sink` type that can create or write data to an "true data source" and fulfills the `Sink` interface (see `?Data.Sink`)
  * Define appropriate `Data.stream!(::Source, ::Sink)` methods as needed between various combinations of Sources and Sinks;
    i.e. define `Data.stream!(::NewPackage.Source, ::CSV.Sink)` and `Data.stream!(::CSV.Source, ::NewPackage.Sink)`
 """
 module DataStreams
 
-export Data
+export Data, PointerString
 module Data
 
+export PointerString
 """
 A custom "weakref" string type that only stores a Ptr{UInt8} and len::Int.
 Allows for extremely efficient string parsing/movement in some cases between files and databases.
@@ -37,25 +38,25 @@ Base.show(io::IO, ::Type{PointerString}) = print(io,"PointerString")
 Base.show(io::IO, x::PointerString) = print(io,x == NULLSTRING ? "PointerString(\"\")" : "PointerString(\"$(bytestring(x.ptr,x.len))\")")
 Base.showcompact(io::IO, x::PointerString) = print(io,x == NULLSTRING ? "\"\"" : "\"$(bytestring(x.ptr,x.len))\"")
 Base.endof(x::PointerString) = x.len
-Base.string(x::PointerString) = x == NULLSTRING ? "" : "$(bytestring(x.ptr,x.len))"
+Base.string(x::PointerString) = x == NULLSTRING ? "" : bytestring(x.ptr,x.len)
 Base.convert(::Type{ASCIIString}, x::PointerString) = convert(ASCIIString, string(x))
 Base.convert(::Type{UTF8String}, x::PointerString) = convert(UTF8String, string(x))
 Base.convert(::Type{PointerString}, x::Union{ASCIIString,UTF8String}) = PointerString(pointer(x.data),sizeof(x))
 
 """
-A `Data.Source` type holds data that can be read/queried/parsed/viewed/streamed; i.e. an "ultimate data source"
+A `Data.Source` type holds data that can be read/queried/parsed/viewed/streamed; i.e. a "true data source"
 To clarify, there are two distinct types of "source":
-  1) the "ultimate data source", which would be the file, database, API, structure, etc; i.e. the actual data
-  2) the `Data.Source` julia object that wraps an "ultimate source" and provides the DataStreams interface
+  1) the "true data source", which would be the file, database, API, structure, etc; i.e. the actual data
+  2) the `Data.Source` julia object that wraps the "true source" and provides the `DataStreams` interface
 
 `Source` types have two different types of constructors:
-  1) "independent constructors" that wrap "ultimate data sources"
-  2) "sink constructors" where a `Data.Sink` object that has received data is turned into a `Source`
+  1) "independent constructors" that wrap "true data sources"
+  2) "sink constructors" where a `Data.Sink` object that has received data is turned into a new `Source` (useful for chaining data processing tasks)
 
 `Source`s also have a, currently implicit, notion of state:
   * `BEGINNING`: a `Source` is in this state immediately after being constructed and is ready to be used; i.e. ready to read/parse/query/stream data from it
   * `READING`: the ingestion of data from this `Source` has started and has not finished yet
-  * `DONE`: the ingestion process has exhausted all data expected from this `Source` instance
+  * `DONE`: the ingestion process has exhausted all data expected from this `Source`
 
 The `Data.Source` interface includes the following:
  * `Data.schema(::Data.Source) => Data.Schema`; typically the `Source` type will store the `Data.Schema` directly, but this isn't strictly required
@@ -63,34 +64,34 @@ The `Data.Source` interface includes the following:
  * `eof(::Data.Source)`; indicates whether the `Source` type is in the `DONE` state; i.e. all data has been exhausted from this source
 
 """
-abstract Source <: IO
+abstract Source
 
+function reset!
+end
 # TODO: flesh out the "fetching"/"reading"/"ingesting" interface for `Source`s
  # getting results: getrow(::Source) => Any[], getcol(::Source) => T[], getfield(::Source) => T; getrows, getcols
  # iterating through results: eachrow(::Source) => Any[], eachcol(::Source) => T[], eachfield(::Source) => T
  # printing/viewing results: printrow(::Source), printcol(::Source), printfield(::Source); printrows, printcols
-function reset!
-end
-function getfield
-end
-function getrow
-end
-function eachfield
-end
-function eachrow
-end
-function printfield
-end
-function printrow
-end
+# function getfield
+# end
+# function getrow
+# end
+# function eachfield
+# end
+# function eachrow
+# end
+# function printfield
+# end
+# function printrow
+# end
 
 """
-A `Data.Sink` type represents a data destination; i.e. an "ultimate data source" such as a database, file, API endpoint, etc.
+A `Data.Sink` type represents a data destination; i.e. an "true data source" such as a database, file, API endpoint, etc.
 
 There are two broad types of `Sink`s:
-  1) "new sinks": an independent `Sink` constructor creates a *new* "ultimate data source" that can be streamed to
-  2) "existing sinks": the `Sink` wraps an already existing "ultimate data source" (or `Source` object that wraps an "ultimate data source").
-    Upon construction of these Sinks, there is no new creation of "ultimate data source"s; the "ulitmate data source" is simply wrapped to replace or append to
+  1) "new sinks": an independent `Sink` constructor creates a *new* "true data source" that can be streamed to
+  2) "existing sinks": the `Sink` wraps an already existing "true data source" (or `Source` object that wraps an "true data source").
+    Upon construction of these Sinks, there is no new creation of "true data source"s; the "ulitmate data source" is simply wrapped to replace or append to
 
 `Sink`s also have notions of state:
   * `BEGINNING`: the `Sink` is freshly constructed and ready to stream data to; this includes initial metadata like column headers
@@ -100,7 +101,7 @@ There are two broad types of `Sink`s:
 The `Data.Sink` interface includes the following:
  * `Data.schema(::Data.Sink) => Data.Schema`; typically the `Sink` type will store the `Data.Schema` directly, but this isn't strictly required
 """
-abstract Sink <: IO
+abstract Sink
 typealias SourceOrSink Union{Source,Sink}
 
 """
@@ -109,6 +110,12 @@ Data transfer typically continues until `eof(source) == true`, i.e. the `Source`
 no longer receive data. See individual `Data.stream!` methods for more details on specific `Source`/`Sink` combinations.
 """
 function stream!#(::Source,::Sink)
+end
+
+# creates a new Data.Sink of type `T` according to `source` schema and streams data to it
+function Data.stream!{T<:Data.Sink}(source::Data.Source, ::Type{T})
+    sink = T(Data.schema(source))
+    return Data.stream!(source,sink)
 end
 
 """
@@ -139,7 +146,7 @@ header(sch::Schema) = sch.header
 types(sch::Schema) = sch.types
 Base.size(sch::Schema) = (sch.rows,sch.cols)
 Base.size(sch::Schema,i::Int) = ifelse(i == 1, sch.rows, ifelse(i == 2, sch.cols, 0))
-importall Base.Operators
+import Base.==
 ==(s1::Schema,s2::Schema) = header(s1) == header(s2) && types(s1) == types(s2) && size(s1) == size(s2)
 
 const MAX_COLUMN_WIDTH = 100
@@ -193,11 +200,17 @@ type Table{T} <: Source
 end
 
 using NullableArrays
+Base.string(x::NullableVector{Data.PointerString}) = NullableArray(UTF8String[x for x in x.values], x.isnull)
 
 # Constructors
 function Table(schema::Schema,other=0)
     rows, cols = size(schema)
     return Table(schema,NullableVector[NullableArray(T, rows) for T in types(schema)],other)
+end
+# define our own default Data.stream! method since Data.Table <: Source
+function Data.stream!(source::Data.Source, ::Type{Data.Table})
+    sink = Data.Table(Data.schema(source))
+    return Data.stream!(source,sink)
 end
 
 Table(types::Vector{DataType},rows::Integer=0,other=0) = Table(Schema(types,rows),other)
@@ -209,6 +222,8 @@ function Table{T}(A::AbstractArray{T,2},header=UTF8String[],other=0)
     header = isempty(header) ? UTF8String["Column$i" for i = 1:cols] : header
     return Table(Schema(header,types,rows),data,other)
 end
+#TODO
+ # define show for Table
 
 # Interface
 # column access
@@ -226,9 +241,7 @@ end
 end # module Data
 end # module DataStreams
 
-# convert to DataFrame
+# convert Data.Table to DataFrame
 if isdefined(:DataFrames)
     DataFrames.DataFrame(dt::DataStreams.Data.Table) = DataFrame(convert(Vector{Any},dt.data),Symbol[symbol(x) for x in DataStreams.Data.header(dt)])
 end
-#TODO
- # define show for Table
