79fcc1fc95399bdb863056e9c6eda7b6a4ffae57
Jacob Quinn <jacob.quinn@domo.com>
2016-08-11 09:32:35 -0600

    Update docs

--- docs/make.jl
+++ docs/make.jl
@@ -2,17 +2,9 @@ using Documenter, DataStreams
 
 makedocs(
     modules = [DataStreams],
-    doctest = false
 )
 
-# Documenter can also automatically deploy documentation to gh-pages.
-# See "Hosting Documentation" and deploydocs() in the Documenter manual
-# for more information.
 deploydocs(
-    deps = Deps.pip(
-            "pygments",
-            "mkdocs",
-            "mkdocs-material",
-            "python-markdown-math"),
+    deps = Deps.pip("mkdocs", "python-markdown-math"),
     repo = "github.com/JuliaData/DataStreams.jl.git"
 )
