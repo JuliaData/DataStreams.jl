f4e59405fc73581e6ab516f9add3afbb02c5f314
Eric Davies <iamed2@gmail.com>
2017-11-14 17:06:08 -0600

    Better NamedTuple constructor on 0.7

--- src/DataStreams.jl
+++ src/DataStreams.jl
@@ -692,7 +692,7 @@ function NamedTuple(sch::Data.Schema{R}, ::Type{S}=Data.Field,
         names = makeunique(Data.header(sch))
 
         sink = @static if isdefined(Core, :NamedTuple)
-                Base.namedtuple(NamedTuple{names}, (allocate(types[i], rows, reference) for i = 1:length(types))...)
+                NamedTuple{names}((allocate(types[i], rows, reference) for i = 1:length(types)))
             else
                 NamedTuples.make_tuple(collect(names))((allocate(types[i], rows, reference) for i = 1:length(types))...)
             end
